2956c249361eb75772d66ee3c30d132cad0a235a
Eddy1008 <galmandeddy@gmail.com>
2023-08-07 20:46:14 +0200

    Affichage Map dans Mapview

--- app/src/main/java/com/openclassrooms/realestatemanager/main/ui/mapview/MapviewFragment.java
+++ app/src/main/java/com/openclassrooms/realestatemanager/main/ui/mapview/MapviewFragment.java
@@ -5,25 +5,67 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
 
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.openclassrooms.realestatemanager.R;
+import com.openclassrooms.realestatemanager.ViewModelFactory;
 import com.openclassrooms.realestatemanager.databinding.FragmentMapviewBinding;
+import com.openclassrooms.realestatemanager.main.MainViewModel;
 
-public class MapviewFragment extends Fragment {
+public class MapviewFragment extends Fragment implements OnMapReadyCallback {
 
+    private MainViewModel mainViewModel;
     private FragmentMapviewBinding binding;
+    private boolean locationPermissionGranted;
+
+    private final ActivityResultLauncher<String> requestPermissionLauncher = registerForActivityResult(
+            new ActivityResultContracts.RequestPermission(),
+            new ActivityResultCallback<Boolean>() {
+                @Override
+                public void onActivityResult(Boolean result) {
+                    if (result) {
+                        locationPermissionGranted = true;
+                        // TODO getDeviceLocation()
+                    } else {
+                        locationPermissionGranted = false;
+                    }
+                }
+            }
+    );
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
 
+        mainViewModel = new ViewModelProvider(requireActivity(), ViewModelFactory.getInstance(getContext())).get(MainViewModel.class);
+        mainViewModel.initPropertyList();
+
         binding = FragmentMapviewBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
-        binding.mapviewFragmentTextviewTitle.setText("Welcome to my MapView");
+        SupportMapFragment mapFragment = (SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.fragment_mapview);
+        if (mapFragment != null) {
+            mapFragment.getMapAsync(this);
+        }
 
         return root;
+    }
+
+    @Override
+    public void onMapReady(@NonNull GoogleMap googleMap) {
+        googleMap.addMarker(new MarkerOptions()
+                .position(new LatLng(0, 0))
+                .title("Marker"));
     }
 }
